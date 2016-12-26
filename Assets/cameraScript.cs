cdd47c4e047766cae23fec612e3d2182ad402a3c
Bibek Panthi <bpanthi977@gmail.com>
2016-12-26 19:58:47 +0545

    Initial Commit First setup of the game

---
+++ Assets/cameraScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraScript : MonoBehaviour {
+	public GameObject lookAt; // Sharpner where the camera has to look i.e rotatate 
+	GameObject camera;
+	// Use this for initialization
+	void Start () {
+		camera = gameObject;
+	}
+
+	float calculateAngle (Vector3 rotation , Vector3 axis) {
+
+		Vector3 dcs = new Vector3 (Mathf.Cos (rotation.x * Mathf.PI / 180f),
+			              Mathf.Cos (rotation.x * Mathf.PI / 180f),
+			              Mathf.Cos (rotation.x * Mathf.PI / 180f));
+		return Vector3.Angle (dcs,axis	);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (lookAt) {
+			//Vector3 axis = camera.transform.position - lookAt.transform.position;
+			//Vector3 rotation = camera.transform.eulerAngles;
+			//Debug.Log (angle);
+			//camera.transform.RotateAround (lookAt.transform.position, axis, calculateAngle (rotation, axis));
+			camera.transform.LookAt(lookAt.transform.position);
+		}
+	}
+}
---
+++ Assets/pencilScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pencilScript : MonoBehaviour {
+
+	public float forceFactor = 0.1f;
+
+	GameObject pencil;
+	Rigidbody rb;
+	Vector3	i0 = new Vector3(0, 1 ,1);
+	Vector3 j0 = new Vector3(1,0,1);
+
+	void Start () {
+		pencil = this.gameObject;
+		rb = pencil.GetComponent<Rigidbody> ();
+	}
+	
+	void FixedUpdate () {
+		float hor = Input.GetAxis ("Horizontal");
+		float ver = Input.GetAxis ("Vertical");
+		// Cancle opposite velocity
+		if (hor * rb.velocity.x < 0) {  // if hor and veclocity are opposite
+			rb.velocity.Set (0, rb.velocity.y, rb.velocity.z);
+		
+		}
+		if (ver * rb.velocity.y < 0)
+			rb.velocity.Set (rb.velocity.x, 0, rb.velocity.z);
+			
+		// Apply force
+		rb.AddForce (forceFactor* hor , forceFactor* ver , 0,ForceMode.Impulse);
+
+		
+
+	}
+}
